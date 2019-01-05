ba56f84253fd159812952615f4e2dad4056e9627
Vitor Costa <vitu86@gmail.com>
2019-01-05 19:16:58 -0300

    Puting pin in the map and updating position until user hold pressed

--- Turista Virtual/Turista Virtual/MapViewController.swift
+++ Turista Virtual/Turista Virtual/MapViewController.swift
@@ -7,22 +7,26 @@
 //
 
 import UIKit
+import MapKit
 
 class MapViewController: UIViewController {
-
+    
+    // MARK: IBOutlets
+    @IBOutlet weak var mapView: MKMapView!
+    
+    // MARK: Private properties
+    private var currentAnnotation:MKPointAnnotation?
+    
+    // MARK: View Controller Override Functions
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        configureMapView()
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         // Hide the navigation bar on the this view controller
         self.navigationController?.setNavigationBarHidden(true, animated: animated)
-        
-        DispatchQueue.main.asyncAfter(deadline: .now() + 5) { // change 2 to desired number of seconds
-            self.performSegue(withIdentifier: "segueToPhotoAlbum", sender: nil)
-        }
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -30,7 +34,44 @@ class MapViewController: UIViewController {
         // Show the navigation bar on other view controllers
         self.navigationController?.setNavigationBarHidden(false, animated: animated)
     }
+    
+    // MARK: Private functions
+    private func configureMapView() {
+        mapView.delegate = self
+        let longGesture = UILongPressGestureRecognizer(target: self, action: #selector(addPinToMap(longGesture:)))
+        mapView.addGestureRecognizer(longGesture)
+    }
+    
+    @objc private func addPinToMap(longGesture: UIGestureRecognizer) {
+        
+        // Define coordinates
+        let touchPoint = longGesture.location(in: mapView)
+        let newCoordinates = mapView.convert(touchPoint, toCoordinateFrom: mapView)
+        
+        if longGesture.state == .began {
+            // If is the begin of the touch, create the annotation and add it to the map
+            currentAnnotation = MKPointAnnotation()
+            currentAnnotation!.coordinate = newCoordinates
+            mapView.addAnnotation(currentAnnotation!)
+        } else if longGesture.state == .changed {
+            // If the user is still holding, update the annotation position
+            if let currentAnnotation = currentAnnotation {
+                currentAnnotation.coordinate = newCoordinates
+            }
+        } else if longGesture.state == .ended {
+            // If the user lift the finger
+            currentAnnotation = nil
+        }
+    }
+    
+}
 
-
+extension MapViewController: MKMapViewDelegate {
+    // MARK: Mapview Delegate Functions
+    func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
+        if let annotation = view.annotation as? MKPointAnnotation {
+            print("Did select: ", annotation.coordinate)
+        }
+    }
 }
 
