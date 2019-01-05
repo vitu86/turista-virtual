81a4c78868b3dc3810148700d2e53d3a256db61c
Vitor Costa <vitu86@gmail.com>
2019-01-05 15:43:17 -0300

    Adding icon assets, configuring launch screen

---
+++ Turista Virtual/Turista Virtual/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Turista Virtual
+//
+//  Created by Vitor Costa on 05/01/19.
+//  Copyright © 2019 Vitor Costa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
